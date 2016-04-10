97941357cbdbb01e724f248284c8d0efe737c238
Serg Bezuglyy <sergbe@outlook.com>
2016-04-10 16:28:09 -0700

    first commit

---
+++ src/main/java/core/Selenium_properties.java
@@ -0,0 +1,67 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium_properties {
+    public static void main(String[] args) {
+
+        String csvFile = "/Users/Serg/Workspace/Title_Validation_CSV/src/main/resources/Test.csv";
+        BufferedReader br = null;
+        String line = null;
+        String SplitBy = ",";
+        String text_case_id = null;
+        String url = null;
+        String title_expected = null;
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        WebDriver driver = new FirefoxDriver();
+
+        try {
+            while ((line = br.readLine()) != null) {
+
+                String[] csv = line.split(SplitBy);
+
+                text_case_id = csv[0];
+                url = csv[1];
+                title_expected = csv[2];
+
+                driver.get(url);
+                driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+                String title_actual = driver.getTitle();
+                System.out.println("");
+                if (title_expected.equals(title_actual)) {
+                    System.out.println("Test Case ID: \t\t" + text_case_id);
+                    System.out.println("URL: \t\t\t" + url);
+                    System.out.println("Title Expected: \t" + title_expected);
+                    System.out.println("Title Actual: \t\t" + title_actual);
+                    System.out.println("Test Case Result: \t" + "PASSED");
+                } else {
+                    System.out.println("Test Case ID: \t\t" + text_case_id);
+                    System.out.println("URL: \t\t\t" + url);
+                    System.out.println("Title Expected: \t" + title_expected);
+                    System.out.println("Title Actual: \t\t" + title_actual);
+                    System.out.println("Test Case Result: \t" + "FAILED");
+                }
+
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/core/Selenium_propertiesTest.java
@@ -0,0 +1,22 @@
+package core;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Serg on 3/28/16.
+ */
+public class Selenium_propertiesTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
